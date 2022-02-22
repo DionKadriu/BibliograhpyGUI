300613e251c1c830ea5b373ff023f718df975d2f
DionKadriu <77997015+DionKadriu@users.noreply.github.com>
2022-02-22 16:34:00 +0000

    Update RefConference.java

--- src/main/java/com/stir/cscu9t4assignment2021/RefConference.java
+++ src/main/java/com/stir/cscu9t4assignment2021/RefConference.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 /**
- * @author 2835267
+ * @author Dion Kadriu
  * The class that handles the refConferences objects
  */
 @XmlRootElement(name = "item")//setting the RefConference class as the attribute for its instances with the name item
